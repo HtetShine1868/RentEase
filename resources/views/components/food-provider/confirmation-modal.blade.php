5d7f03e6615c61640d718899d5a30c5913a9ad59
Hsu TD <hsuthada2007@gmail.com>
2026-01-27 12:50:32 +0630

    confirmation-modal.blade.php

--- resources/views/components/food-provider/confirmation-modal.blade.php
+++ resources/views/components/food-provider/confirmation-modal.blade.php
@@ -42,8 +42,9 @@
     
     <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
         <button type="button"
-                @click="{{ $onConfirm ? $onConfirm : 'closeModal(\'' . $id . '\')' }}"
-                class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 {{ $confirmColorClass }} text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">
+            data-role="confirm"
+            @click="{{ $onConfirm ? $onConfirm : 'closeModal(\'' . $id . '\')' }}"
+            class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 {{ $confirmColorClass }} text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm">
             {{ $confirmText }}
         </button>
         <button type="button"
